d481117108628cf3c89b0c4af5c38200213a87a2
Migufe <joaopfc2764@gmail.com>
2023-01-19 12:24:06 -0300

    refactor

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,9 +7,6 @@ import { Container } from "./styles";
 
 export function Summary() {
   const transactions = useContext(TransactionsContext)
-
-  console.log(transactions)
-
   return (
     <Container>
       <div>
